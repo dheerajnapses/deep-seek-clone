9fa279e12bf3622fd3c52b3a87e73bb6a2ea29a8
Dheeraj Agrahari <dheeraj.a@napses.com>
2025-06-08 18:57:19 +0530

    jgj

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -9,23 +9,23 @@ export function middleware(req: NextRequest) {
   const userCookie = req.cookies.get('auth_token');
   console.log(userCookie)
 
-  // Check if user has a cookie and trying to access a public route like sign-in or sign-up
-  if ((pathname === '/sign-in' || pathname === '/sign-up') && userCookie) {
-    console.log('Redirecting authenticated user from public route');
-    return NextResponse.redirect(new URL('/', req.url)); // Redirect authenticated users to home page
-  }
+  // // Check if user has a cookie and trying to access a public route like sign-in or sign-up
+  // if ((pathname === '/sign-in' || pathname === '/sign-up') && userCookie) {
+  //   console.log('Redirecting authenticated user from public route');
+  //   return NextResponse.redirect(new URL('/', req.url)); // Redirect authenticated users to home page
+  // }
 
-  // If the route is a public route, allow the request to continue
-  if (publicRoutes.has(pathname)) {
-    console.log('Public route, allowing access');
-    return NextResponse.next(); // Proceed to the route
-  }
+  // // If the route is a public route, allow the request to continue
+  // if (publicRoutes.has(pathname)) {
+  //   console.log('Public route, allowing access');
+  //   return NextResponse.next(); // Proceed to the route
+  // }
 
-  // If the user is not authenticated and not accessing a public route, redirect to sign-in
-  if (!userCookie) {
-    console.log('User not authenticated, redirecting to sign-in');
-    return NextResponse.redirect(new URL('/sign-in', req.url)); // Redirect to login
-  }
+  // // If the user is not authenticated and not accessing a public route, redirect to sign-in
+  // if (!userCookie) {
+  //   console.log('User not authenticated, redirecting to sign-in');
+  //   return NextResponse.redirect(new URL('/sign-in', req.url)); // Redirect to login
+  // }
 
   // Allow the user to continue to the page if everything is okay
   return NextResponse.next();
@@ -33,11 +33,11 @@ export function middleware(req: NextRequest) {
 
 // Define which routes the middleware should apply to
 export const config = {
-  matcher: [
-    "/sign-in", 
-    "/sign-up", 
-    "/forgot-password", 
-    "/", 
-    "/chat/:path*" 
-  ]
+  // matcher: [
+  //   "/sign-in", 
+  //   "/sign-up", 
+  //   "/forgot-password", 
+  //   "/", 
+  //   "/chat/:path*" 
+  // ]
 };
