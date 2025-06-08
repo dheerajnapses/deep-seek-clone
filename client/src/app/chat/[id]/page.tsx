df27850c26853aefdd58d80710ea33063c081d15
Dheeraj Agrahari <dheeraj.a@napses.com>
2025-06-08 18:46:51 +0530

    f

--- client/src/app/chat/[id]/page.tsx
+++ client/src/app/chat/[id]/page.tsx
@@ -2,5 +2,6 @@ import Conversations from "./Conversations";
 
 export default  function ChatPage({ params }: { params: { id: string } }) {
   const { id } = params; 
+  //
   return <Conversations id={id} />;
 }
