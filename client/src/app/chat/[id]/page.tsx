a271c881809a03cd1bacca8b58d66c2a6aa0aae9
Dheeraj Agrahari <dheeraj.a@napses.com>
2025-06-08 18:25:11 +0530

    fixed some code

--- client/src/app/chat/[id]/page.tsx
+++ client/src/app/chat/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Conversations from "./Conversations";
 
 export default async function ChatPage({ params }: { params: { id: string } }) {
-  const { id } = await params; 
+  const { id } = params; 
   return <Conversations id={id} />;
 }
